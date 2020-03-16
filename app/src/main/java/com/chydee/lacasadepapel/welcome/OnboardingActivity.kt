0799a5c5b76bf351c07c921c146c75f6d7066db6
Chydee <desmondmuqsit@yahoo.com>
2020-03-16 21:51:01 +0100

    set up OnBoardingActivity

--- app/src/main/java/com/chydee/lacasadepapel/welcome/OnboardingActivity.kt
+++ app/src/main/java/com/chydee/lacasadepapel/welcome/OnboardingActivity.kt
@@ -2,12 +2,16 @@ package com.chydee.lacasadepapel.welcome
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.chydee.lacasadepapel.R
+import com.chydee.lacasadepapel.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+
+        val binding: ActivityOnboardingBinding = ActivityOnboardingBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
     }
 }
