aec773a35397583bd116ae2b12981c73e6770dcd
Chydee <desmondmuqsit@yahoo.com>
2020-03-25 14:03:46 +0100

    working fine

--- app/src/main/java/com/chydee/lacasadepapel/welcome/OnBoardFragment.kt
+++ app/src/main/java/com/chydee/lacasadepapel/welcome/OnBoardFragment.kt
@@ -1,7 +1,6 @@
 package com.chydee.lacasadepapel.welcome
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,16 +10,12 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.chydee.lacasadepapel.R
 import com.chydee.lacasadepapel.databinding.OnBoardFragmentBinding
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 const val TAG = "OnBoardFragment"
 
 class OnBoardFragment : Fragment() {
 
     private lateinit var viewModel: OnBoardViewModel
-    private var db: FirebaseFirestore? = null
     private lateinit var binding: OnBoardFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,30 +23,16 @@ class OnBoardFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.on_board_fragment, container, false)
         viewModel = ViewModelProvider(this).get(OnBoardViewModel::class.java)
-        setupDB()
-        binding.continueButton.setOnClickListener {
-            findNavController().navigate(OnBoardFragmentDirections.actionOnBoardFragmentToWelcomeUserFragment())
-        }
-
-        return binding.root
-    }
-
-    private fun setupDB() {
-        // Access a Cloud Firestore instance from your Activity
-        db = Firebase.firestore
         val player = hashMapOf(
             "name" to binding.editTextPlayerName.text.toString(),
             "score" to 0
         )
+        binding.continueButton.setOnClickListener {
+            viewModel.setUpDb(player)
+            findNavController().navigate(OnBoardFragmentDirections.actionOnBoardFragmentToWelcomeUserFragment())
+        }
 
-        db!!.collection("players")
-            .add(player)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding Document", e)
-            }
+        return binding.root
     }
 
 }
--- app/src/main/java/com/chydee/lacasadepapel/welcome/OnBoardViewModel.kt
+++ app/src/main/java/com/chydee/lacasadepapel/welcome/OnBoardViewModel.kt
@@ -1,8 +1,27 @@
 package com.chydee.lacasadepapel.welcome
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class OnBoardViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    val db = Firebase.firestore
+
+
+    fun setUpDb(player: Any) {
+        db.collection("players")
+            .add(player)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding Document", e)
+            }
+    }
+
+
 }
 
