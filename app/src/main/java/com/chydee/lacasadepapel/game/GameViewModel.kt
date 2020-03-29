698223dfc58ad6fc93d4f1d6fd8a4fbeb20680d9
Chydee <desmondmuqsit@yahoo.com>
2020-03-29 07:27:59 +0100

    updated

--- app/src/main/java/com/chydee/lacasadepapel/game/GameViewModel.kt
+++ app/src/main/java/com/chydee/lacasadepapel/game/GameViewModel.kt
@@ -13,11 +13,8 @@ class GameViewModel : ViewModel() {
     private var db: FirebaseFirestore = Firebase.firestore
 
     fun getQuiz(): Task<QuerySnapshot> {
-        // The default cache size threshold is 100 MB. Configure "setCacheSizeBytes"
-        // for a different threshold (minimum 1 MB) or set to "CACHE_SIZE_UNLIMITED"
-        // to disable clean-up.
         val settings = FirebaseFirestoreSettings.Builder()
-            .setCacheSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED)
+            .setPersistenceEnabled(true)
             .build()
         db.firestoreSettings = settings
         val quizDocRef = db.collection("quizes")
